c221e46dd705718d3c337467b7a87fe32db78080
Luca Zanzottera <lzanzottera@gmail.com>
2019-10-17 14:50:16 +0200

    cleaning workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,12 @@ module.exports = {
   plugins: [
     'prettier'
   ],
+
   // add your custom rules here
   rules: {
     "vue/component-name-in-template-casing": ["error", "PascalCase"],
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "prettier/prettier": "error"
   }
 }
