2a8de9c3e70514a22031b130882979bff3a5bff5
Luca <lzanzottera@gmail.com>
2019-12-04 18:16:54 +0100

    added wtf library

--- components/ElementPanel.vue
+++ components/ElementPanel.vue
@@ -23,6 +23,7 @@
   </div>
 </template>
 <script>
+import wtf from 'wtf_wikipedia'
 import { mapState } from 'vuex'
 import helpersGraph from '~/mixins/helpersGraph'
 import helpersFunctions from '~/mixins/helpersFunctions.js'
@@ -57,108 +58,78 @@ export default {
   },
   watch: {
     selectedElement: async function(newSelectedElement) {
-      console.group('watch selectedElement')
-      const query = this.queryPlacesByName(newSelectedElement)
-      const url = this.getDataEndpoint(this.language, CMS, QUERY) + GRAPHQL_URI
-
-      try {
-        const response = await this.axiosCall(url, 'post', { query })
-        console.dir(response)
-        const placesFound = response.data.data.places
-
-        if (placesFound.length) {
-          console.log(placesFound[0])
-          this.cmsItem.Identifier = placesFound[0].Identifier
-          this.cmsItem.Name = placesFound[0].Name
-          this.cmsItem.Description = placesFound[0].Description
-        } else {
-          console.log('Non ho trovato niente su CMS')
-          const url =
-            this.getDataEndpoint(this.language, WIKIPEDIA, CONTENT_SEARCH) +
-            newSelectedElement
-
-          const response = await this.axiosCall(url, 'get')
-
-          if (typeof response !== 'undefined') {
-            const page = Object.keys(response.data.query.pages)[0]
-
-            this.wikiItem.Description = response.data.query.pages[page].extract
-          } else {
-          }
-        }
-      } catch (error) {
-        console.log('ramo catch watcher selectedElement')
-        throw error
-      }
+      console.group('watcher selectedElement')
+     // const result= await this.getData(CMS, QUERY,'post') 
+      //if(!result){
+       await this.getData(WIKIPEDIA, CONTENT_SEARCH) 
+     // }
+    
     }
   },
   methods: {
-    async getData(source, operation) {
-      let query, uri, response, retrievedData, responseFormat, destObj
+    async getData(source, operation, method) {
+      console.log ('cazzo di source ',source)
+      let query, uri, response, retrievedData, responseFormat, destObj,itemfound
 
       if (source === CMS) {
         uri = GRAPHQL_URI
-        method = 'post'
         responseFormat = 'data.places'
         destObj = this.cmsItem
         query = this.queryPlacesByName(this.selectedElement)
-
+		
+		const url = this.getDataEndpoint(this.language, source, operation) + uri
+		console.log('url ', url)
+		console.log('uri ', uri)
+      console.log('method ', method)
+      console.log('responseFormat ', responseFormat)
+      console.log('query ', query)
+		 try {
+				response = await this.axiosCall(url, method, { query })
+			  } catch {
+				console.log('catch chiamata axios a CMS')
+				return null
+			  }
+	  
+       console.log('RESPONSE')
+      console.log(response)
+	  
+      retrievedData = response.data
+	  
+      console.log('retrievedData')
+      console.log(retrievedData)
+	  
+      itemfound = this.getProp(retrievedData, responseFormat)
+      console.log('itemfound')
+      console.log(itemfound)
+	  if(itemfound){
+          this.cmsItem.Identifier = itemfound[0].Identifier
+          this.cmsItem.Name = itemfound[0].Name
+          this.cmsItem.Description = itemfound[0].Description
+          return true
         //const placesFound = retrievedData['data']['places']
-      } else if (source === WIKIPEDIA) {
-        uri = newSelectedElement
-        method = 'get'
-        responseFormat = 'query.pages'
-        destObj = this.wikiItem
+		} 
+	  
+	  
+	  
+	  
+	  }	  
+	  else if (source === WIKIPEDIA) {
+console.log('ramo wiki')
+
+//console.dir(this.wikiItem)
+//this.wikiItem.Description =await wtf(this.selectedElement);
+     this.wikiItem.Description = await wtf.fetch(this.selectedElement, this.language)
+		
+       console.log(this.wikiItem.Description.text())
+      
+
+        // responseFormat = 'query.pages'
+        // destObj = this.wikiItem
       } else {
         console.log('source not valid')
         return null
       }
-      console.log('uri ', uri)
-      console.log('method ', method)
-      console.log('responseFormat ', responseFormat)
-      console.log('query ', query)
-      console.dir(destObj)
-
-      const url = this.getDataEndpoint(this.language, source, operation) + uri
-      console.log('url ', url)
-      try {
-        response = await this.axiosCall(url, method, { query })
-      } catch {
-        console.log('catch chiamata axios di recupero dati')
-      }
-      console.log(response)
-      retrievedData = response.data
-      console.log(retrievedData)
-      itemfound = this.getProp(retrievedData, responseFormat)
-      console.log(itemfound)
-
-      if (itemfound) {
-        if (source === CMS) {
-          console.log(itemfound[0])
-          this.cmsItem.Identifier = itemfound[0].Identifier
-          this.cmsItem.Name = itemfound[0].Name
-          this.cmsItem.Description = itemfound[0].Description
-        } else if (source === WIKIPEDIA) {
-          const page = Object.keys(itemfound)[0]
-          this.wikiItem.Description = itemfound[page].extract
-        }
-        // this.wikiItem.Description = retrievedData['query']['pages'][page].extract
-      }
-
-      // if (placesFound.length) {
-      //  console.log(placesFound[0])
-      //  this.cmsItem.Identifier = placesFound[0].Identifier
-      //  this.cmsItem.Name = placesFound[0].Name
-      //  this.cmsItem.Description = placesFound[0].Description
-
-      // }elsif  (source === 'WIKI'){
-      //uri = newSelectedElement
-
-      // if (typeof response !== 'undefined') {
-      // const page = Object.keys(retrievedData['query']['pages'])[0]
-
-      // this.wikiItem.Description = retrievedData['query']['pages'][page].extract
-    }
+     }
   }
 }
 </script>
