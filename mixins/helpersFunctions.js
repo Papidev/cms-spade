1425c7d3bf4667171a65d0244203a5fdc627a653
Luca <lzanzottera@gmail.com>
2019-12-16 17:59:04 +0100

    Update helpersFunctions.js

--- mixins/helpersFunctions.js
+++ mixins/helpersFunctions.js
@@ -1,20 +1,5 @@
 export default {
   methods: {
-    async axiosCall(url, method, data) {
-      //
-      let response
-      try {
-        if (method === 'get') {
-          response = await this.$axios.$get(url)
-        } else {
-          response = await this.$axios.$post(url, data)
-        }
-      } catch (error) {
-        throw error
-      }
-
-      return response
-    },
 
     getDataEndpoint(lang, source, operation) {
       //
