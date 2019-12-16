5360b94166d229adddf787c99ad067a84a76406a
Luca <lzanzottera@gmail.com>
2019-12-16 17:58:31 +0100

    Update wikipedia.js

--- constants/wikipedia.js
+++ constants/wikipedia.js
@@ -1,4 +1,4 @@
-export const WIKIPEDIA = 'wikipedia'
-export const SUGGEST_SEARCH = 'opensearch'
-export const SUGGEST_SEARCH_CHARS = 4
-export const CONTENT_SEARCH = 'query'
+export const WIKI = 'wikipedia'
+export const WIKI_SUGGEST_SEARCH = 'opensearch'
+export const WIKI_SUGG_SEARCH_CHARS = 4
+export const WIKI_CONTENT_SEARCH = 'query'
