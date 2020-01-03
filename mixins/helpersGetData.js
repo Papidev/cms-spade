4416c06ecbb945f6e7d3d2b5e72396ae8487e850
Luca <lzanzottera@gmail.com>
2020-01-03 17:26:02 +0100

    Update helpersGetData.js

--- mixins/helpersGetData.js
+++ mixins/helpersGetData.js
@@ -1,19 +1,19 @@
-// export default {
-//   methods: {
-//     async axiosCall(url, method, data) {
-//       //
-//       let response
-//       try {
-//         if (method === 'get') {
-//           response = await this.$axios.$get(url)
-//         } else {
-//           response = await this.$axios.$post(url, data)
-//         }
-//         // eslint-disable-next-line no-empty
-//       } finally {
-//       }
+export default {
+  methods: {
+    async axiosCall(url, method, data) {
+      //
+      let response
+      try {
+        if (method === 'get') {
+          response = await this.$axios.$get(url)
+        } else {
+          response = await this.$axios.$post(url, data)
+        }
+        // eslint-disable-next-line no-empty
+      } finally {
+      }
 
-//       return response
-//     }
-//   }
-// }
+      return response
+    }
+  }
+}
