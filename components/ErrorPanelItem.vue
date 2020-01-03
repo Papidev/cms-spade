777cb21d471c51614a365c8e2729f29464bf5ed9
Luca <lzanzottera@gmail.com>
2020-01-03 17:24:48 +0100

    Create ErrorPanelItem.vue

---
+++ components/ErrorPanelItem.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bg-red-200">
+    {{ errorInfo.errorStep }} : {{ errorInfo.errorMessage }}
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    errorId: {
+      required: true,
+      type: Number
+    },
+    errorInfo: {
+      required: true,
+      type: Object
+    }
+  },
+
+  mounted() {
+    //  this.$emit('errorShown', this.errorId)
+  }
+}
+</script>
